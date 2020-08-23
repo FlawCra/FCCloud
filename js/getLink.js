daa1e22cc55b72dfd31248fee57a49f753f9dc3e
FlawCra <ildi251086@gmail.com>
2020-08-23 16:33:45 +0200

    Changed a few things
    Chaned styles
    Download button is now working

--- js/getLink.js
+++ js/getLink.js
@@ -1,1 +1,8 @@
 //(c) 2020 FlawCra
+var dlBtn = document.getElementById("dlBtn");
+var dlPane = document.getElementById("dlPane");
+$.get( "http://api.flawcra.cc/fcmod/api/download", function( data ) {
+  dlBtn.href = data;
+  dlBtn.style.display = "block";
+  dlPane.style.display = "none";
+});
