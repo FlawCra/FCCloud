aa298ec246ee080ab513e2e57461bebf2a1bf0bb
FlawCra <ildi251086@gmail.com>
2020-08-23 16:56:08 +0200

    Update getLink.js

--- js/getLink.js
+++ js/getLink.js
@@ -1,8 +1,18 @@
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
 //(c) 2020 FlawCra
 var dlBtn = document.getElementById("dlBtn");
 var dlPane = document.getElementById("dlPane");
 $.get( "https://api.flawcra.cc/fcmod/api/download/", function( data ) {
-	dlBtn.setAttribute("onclick", 'open("'+data+'");');
+	dlBtn.setAttribute("onclick", 'downloadURI("'+data+'", "'+data.substring(data.lastIndexOf('/')+1)+'");');
 	dlBtn.style.display = "block";
 	dlBtn.style.cursor = "pointer";
 	dlPane.style.display = "none";
